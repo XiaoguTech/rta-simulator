02837458fac3d4692b14a12641d367f1cba76307
Lee <ljz@xiaogu-tech.com>
2017-11-21 11:02:57 +0800

    alter main.py

--- main.py
+++ main.py
@@ -10,32 +10,31 @@ log = sys.stdout.write
 error = sys.stderr.write
 
 class Config:
-    def __init__(self, download_url, download_file, result_file, \
-                    tag_keys, tag_values, field_keys, field_values, split_symbol=';', src='?', dest='0.000', fromline = 1,\
-                    batch_size=1, start_time=None, interval=10,
-                    hosts='https://twinpines-9429794e.influxcloud.net:8086', database='house_data', username='xiaogu', password='123q456w', measurement='house'):
-        self.download_url = download_url
-        self.download_file = download_file
-        self.result_file = result_file
-        self.split_symbol = split_symbol
+    def __init__(self):        
+        self.download_url = get_env('RTA_URL', 'http://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip')
+        self.download_file = get_env('RTA_FILE', 'household_power_consumption.txt')
+        self.result_file = get_env('RTA_RESULT', 'house.influxdb')
+        self.split_symbol = get_env('RTA_SPLIT_SYMBOL', ';')
+
+        self.tag_keys = get_env('RTA_TAG_KEYS', 'metric').split(' ')
+        self.tag_values = get_env('RTA_TAG_VALUES', 'ljz').split(self.split_symbol)
+        self.field_keys = get_env('RTA_FIELD_KEYS', 'Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;Sub_metering_3').split(self.split_symbol)
+        self.field_values = get_env('RTA_FIELD_VALUES', '$3 $4 $5 $6 $7 $8 $9').split(self.split_symbol)
         
-        self.tag_keys = tag_keys.split(' ')
-        self.tag_values = tag_values.split(',')
-        self.field_keys = field_keys.split(';')
-        self.field_values_pos = [res[1:] for res in field_values.split(' ')]
-        self.src = src
-        self.dest = dest
-        self.fromline = fromline
+        # self.field_values_pos = [res[1:] for res in field_values.split(' ')]
+        self.replace_src = get_env('RTA_REPLACE_SRC', '?')
+        self.replace_dest = get_env('RTA_REPLACE_DEST', '0.000')
+        self.fromline = int(get_env('RTA_FROMLINE', '2')) - 1
 
-        self.batch_size = batch_size
-        self.start_time = start_time
-        self.interval = interval
+        self.batch_size = int(get_env('RTA_BATCH_SIZE', '1'))
+        self.start_time = get_env('RTA_START_TIME')
+        self.interval = int(get_env('RTA_INTERVAL', '10'))
 
-        self.hosts = hosts
-        self.database = database
-        self.measurement = measurement
-        self.username = username
-        self.password = password
+        self.hosts = get_env('RTA_INFLUX_HOST', 'https://twinpines-9429794e.influxcloud.net:8086')
+        self.database = get_env('RTA_DATABASE', 'house')
+        self.measurement = get_env('RTA_MEASUREMENT', 'house')
+        self.username = get_env('RTA_USERNAME', 'xiaogu')
+        self.password = get_env('RTA_PASSWORD', '123q456w')
 
 class Simulator:
     def __init__(self, config):
@@ -68,16 +67,20 @@ class Simulator:
                 
                 for line in reader:
                     values = line.split(self.config.split_symbol)
+                    # diff the immediate num and x'th column
                     fields = reduce(lambda x, y: x+'%s=%s,' % (y[0], values[int(y[1])-1]), list(zip(self.config.field_keys, self.config.field_values_pos)), '')[:-1]
                     
-                    # it should be a loop
+                    # every series is a tag
                     for tag in tags:
                         line_data = protocol_format.format(tags=tag[:-1], fields=fields).replace(self.config.src, self.config.dest)
                         log(line_data)
                         writer.write(line_data)
 
     def upload(self):
-        upload_url = '%s/write?db=%s&u=%s&p=%s' % (self.config.hosts, self.config.database, self.config.username, self.config.password)
+        if self.config.username == '':
+            upload_url = '%s/write?db=%s' % (self.config.hosts, self.config.database)            
+        else:
+            upload_url = '%s/write?db=%s&u=%s&p=%s' % (self.config.hosts, self.config.database, self.config.username, self.config.password)
         with open(self.config.result_file) as reader:
             count = 0
             text = ''
@@ -90,7 +93,7 @@ class Simulator:
                     text = ''
                 else:
                     count += 1
-                    text += line
+                    text += line 
                                 
 
 class Director:
@@ -109,14 +112,6 @@ def get_env(key, default=None):
         return default
 
 if __name__ == "__main__":
-    download_url = get_env('RTA_URL', 'http://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip')
-    download_file = get_env('RTA_FILE', 'household_power_consumption.txt')
-    result_file = get_env('RTA_RESULT', 'house.influxdb')
-    tag_keys = get_env('RTA_TAG_KEYS', 'metric')
-    tag_values = get_env('RTA_TAG_VALUES', 'ljz')
-    field_keys = get_env('RTA_FIELD_KEYS', 'Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;Sub_metering_3')
-    field_values = get_env('RTA_FIELD_VALUES', '$3 $4 $5 $6 $7 $8 $9')
-    config = Config(download_url=download_url, download_file=download_file, result_file=result_file, tag_keys=tag_keys, tag_values=tag_values, \
-                    field_keys=field_keys, field_values=field_values)
+    config = Config()
     Director.Start(Simulator(config))
     
